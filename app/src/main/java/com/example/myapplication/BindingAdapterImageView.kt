4fb5c5850d86d442c03084af044fa393c98c2639
Adiya <garudaditya7@gmail.com>
2023-03-12 18:47:54 +0530

    initial commit

--- app/src/main/java/com/example/myapplication/BindingAdapterImageView.kt
+++ app/src/main/java/com/example/myapplication/BindingAdapterImageView.kt
@@ -5,6 +5,6 @@ import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
 @BindingAdapter("loadImageFromUrl")
-fun ImageView.loadImageFromUrl(url: String?){
+fun ImageView.loadImageFromUrl(url: String){
     Glide.with(this.context).load(url).into(this)
 }
