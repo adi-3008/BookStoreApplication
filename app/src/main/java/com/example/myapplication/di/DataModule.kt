7983ec03b972603054889b3fa70216b24d13d9b1
Adiya <garudaditya7@gmail.com>
2023-03-25 21:36:56 +0530

    Use of dependency injection framework

--- app/src/main/java/com/example/myapplication/di/DataModule.kt
+++ app/src/main/java/com/example/myapplication/di/DataModule.kt
@@ -6,10 +6,13 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 abstract class DataModule {
+
+    @Singleton
     @Binds
     abstract fun provideDataSource(data: Data) : DataSourceInterface
 }
--- app/src/main/java/com/example/myapplication/repository/BookRepository.kt
+++ app/src/main/java/com/example/myapplication/repository/BookRepository.kt
@@ -5,31 +5,16 @@ import androidx.lifecycle.MutableLiveData
 import com.example.myapplication.api.DataSourceInterface
 import com.example.myapplication.model.Book
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class BookRepository @Inject constructor(private var dataSource: DataSourceInterface){
 
     private var _bookList: MutableLiveData<List<Book>> = MutableLiveData()
     val bookList: LiveData<List<Book>> get() = _bookList
 
     suspend fun getBooks(){
-        if (DataCache.getBooks() != null){
-            _bookList.value = DataCache.getBooks()
-            return
-        }
         _bookList.value = dataSource.getBooks()
-        DataCache.setBooks(_bookList.value!!)
-    }
-}
-
-object DataCache{
-    private var bookList : List<Book>? = null
-
-    fun setBooks(bookList: List<Book>){
-        this.bookList = bookList
-    }
-
-    fun getBooks() : List<Book>?{
-        return bookList
     }
 }
 
